e26765b384064c251a6b7d2fc3322f0131ef9ffc
André Carvalho <andre_carvalho0@live.com>
2020-11-05 19:39:26 -0300

    Correção de Bugs inimigo
    Criação de funcionalidades dos inimigos

--- NinjaGo/script_inimigo.gd
+++ NinjaGo/script_inimigo.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-var velocidade = 200
+var tipo_premio = "trofeu"
+var velocidade = 250
 var direcao = 1 # Direcao 1 para direita, -1 para esquerda
 var mov = Vector2(velocidade,0)
 
@@ -15,33 +16,23 @@ func _ready():
 		$Zumbi1.visible = true
 	if ScriptGlobal.cod_inimigo == 3:
 		$Abobora.visible = true
-
-func _process(delta):
-	mov.x = direcao * velocidade	
-	mov = move_and_slide(mov)
+	
 	andando()
 	if(not ScriptGlobal.zombi and ScriptGlobal.status_efeitos_sonoros):
 		$Zombi.play()
 
+func _process(delta):
+	mov.x = direcao * velocidade	
+	mov = move_and_slide(mov)
+
 func _on_pe_esquerdo_body_exited(body):
+	direcao = 1
 	flipF()
 	
 func _on_pe_direito_body_exited(body):
+	direcao = -1
 	flipT()
-
-func _on_Ataque_body_entered(body):
-	if(direcao==1):
-		flipF()
-	else:
-		flipT()
-		
-func premio():
-	var cena_premio = preload("res://cena_premio.tscn")
-	var objeto = cena_premio.instance()
-	ScriptGlobal.troc_premio()
-	objeto.global_position = global_position
-	get_parent().get_parent().add_child(objeto)
-
+	
 func _on_Timer_timeout():
 	disparo()
 	
@@ -54,6 +45,47 @@ func disparo():
 			objeto_tiro.scale.x = -1
 		objeto_tiro.global_position = $Position2D.global_position
 		get_tree().root.add_child(objeto_tiro)
+
+func _on_AnimatedSprite_animation_finished():
+	if ($AnimatedSprite.animation=="morrendo"):
+		queue_free()
+		premio()
+	ScriptGlobal.zombi = false
+
+func _on_Zumbi2_animation_finished():
+	if ($Zumbi2.animation=="morrendo"):
+		queue_free()
+		premio()
+		ScriptGlobal.zombi = false
+	
+func _on_Zumbi_animation_finished():
+	if ($Zumbi.animation=="morrendo"):
+		queue_free()
+		premio()
+		ScriptGlobal.zombi = false
+
+func _on_Abobora_animation_finished():
+	if ($Abobora.animation=="morrendo"):
+		queue_free()
+		premio()
+		ScriptGlobal.zombi = false
+
+func _on_Ataque_body_entered(body):
+	if (body.name=="Personagem" and ScriptGlobal.atacando == false):
+		ScriptGlobal.qtd_vidas -= 1
+		ScriptGlobal.zombi = true
+		direcao = direcao * (-1)
+	if(direcao==1):
+		flipF()
+	else:
+		flipT()
+		
+func premio():
+	var cena_premio = preload("res://cena_premio.tscn")
+	var objeto = cena_premio.instance()
+	ScriptGlobal.troc_premio()
+	objeto.global_position = global_position
+	get_parent().get_parent().add_child(objeto)
 	
 func morrendo():
 	if ScriptGlobal.cod_inimigo == 1:
@@ -65,35 +97,12 @@ func morrendo():
 
 func andando():
 	if ScriptGlobal.cod_inimigo == 1:
-		return $Zumbi.play("morrendo")
+		return $Zumbi.play("andando")
 	if ScriptGlobal.cod_inimigo == 2:
-		return $Zumbi2.play("morrendo")
+		return $Zumbi2.play("andando")
 	if ScriptGlobal.cod_inimigo == 3:
-		return $Abobora.play("morrendo")
-
-func _on_Zumbi2_animation_finished():
-	animation()
-	premio()
-	ScriptGlobal.zombi = false
-	
-func _on_Zumbi_animation_finished():
-	animation()
-	premio()
-	ScriptGlobal.zombi = false
-
-func _on_Abobora_animation_finished():
-	animation()
-	premio()
-	ScriptGlobal.zombi = false
-
-func animation():
-	if ScriptGlobal.cod_inimigo == 1 and $Zumbi.animation=="morrendo":
-		return queue_free()
-	if ScriptGlobal.cod_inimigo == 2 and $Zumbi2.animation=="morrendo":
-		return queue_free() 
-	if ScriptGlobal.cod_inimigo == 3 and $Abobora.animation=="morrendo":
-		return queue_free()
-
+		return $Abobora.play("andando")
+		
 func flipF():
 	if ScriptGlobal.cod_inimigo == 1:
 		$Zumbi.flip_h = false
